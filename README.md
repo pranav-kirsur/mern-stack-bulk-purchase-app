913d315c337ca02c1fd0d81b4f036b6e2ae56053
pranav-kirsur <pranav.kirsur@gmail.com>
2021-02-04 20:59:15 +0530

    forgot to add some stuff

---
+++ README.md
@@ -0,0 +1,7 @@
+First run npm install in  both backend and frontend
+
+Backend:
+nodemon run server
+
+Frontend:
+npm start
--- frontend/src/components/dispatched-orders-display.component.js
+++ frontend/src/components/dispatched-orders-display.component.js
@@ -10,41 +10,50 @@ export default class DispatchedOrdersDisplay extends Component {
   }
 
   componentDidMount() {
-    axios
-      .get(
-        "http://localhost:4000/api/product/getbystatus/dispatched/" +
-          localStorage.getItem("UserID")
-      )
-      .then(response => {
-        let orders = response.data;
-        this.dispatched_orders = new Array(orders.length, []);
-        for (let i in orders) {
-          let order = orders[i];
-          axios
-            .get("http://localhost:4000/api/order/getbyproductid/" + order._id)
-            .then(response => {
-              this.dispatched_orders[i] = response.data;
-            })
-            .catch(function(error) {
-              console.log(error);
-            });
-        }
+    if (!this.state.refresh) {
+      axios
+        .get(
+          "http://localhost:4000/api/product/getbystatus/dispatched/" +
+            localStorage.getItem("UserID")
+        )
+        .then(response => {
+          let orders = response.data;
+          this.dispatched_orders = new Array(orders.length);
+          for (let i in orders) {
+            this.dispatched_orders[i] = [];
+          }
+          for (let i in orders) {
+            let order = orders[i];
+            axios
+              .get(
+                "http://localhost:4000/api/order/getbyproductid/" + order._id
+              )
+              .then(response => {
+                this.dispatched_orders[i] = response.data;
+              })
+              .catch(function(error) {
+                console.log(error);
+              });
+          }
 
-        for (let i in this.dispatched_orders) {
-          if (this.dispatched_orders[i].length > 0)
-            this.render_orders = this.render_orders.concat(
-              this.dispatched_orders[i]
-            );
-        }
+          for (let i in this.dispatched_orders) {
+              console.log("")
+            for(let j in this.dispatched_orders[i])
+            {
+                console.log(this.dispatched_orders[i][j])
+                this.render_orders.append(this.dispatched_orders[i][j])
+            }
+          }
 
-        console.log(this.dispatched_orders);
-        console.log("renders");
-        console.log(this.render_orders);
-        this.setState({ refresh: true });
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
+        //   console.log(this.dispatched_orders);
+        //   console.log("renders");
+        //   console.log(this.render_orders);
+          this.setState({ refresh: true });
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
   }
 
   render() {
